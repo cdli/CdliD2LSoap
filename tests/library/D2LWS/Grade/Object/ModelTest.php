667a1cfef434620c394a6c423fd46062e2752c29
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-06-08 15:01:35 -0230

    Extended test suite for D2lWS_Grade_Object_Model

--- tests/library/D2LWS/Grade/Object/ModelTest.php
+++ tests/library/D2LWS/Grade/Object/ModelTest.php
@@ -29,6 +29,30 @@
  */
 class D2LWS_Grade_Object_ModelTest extends GenericTestCase
 {
+    /**
+     *
+     * @var D2LWS_Grade_Object_Model 
+     */
+    protected $baseObj;
+    
+    /**
+     * 
+     * @var D2LWS_Grade_Object_Model 
+     */
+    protected $testObj;
+        
+    /**
+     * Pre-Test Setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->baseObj = new D2LWS_Grade_Object_Model();
+        $this->testObj = new D2LWS_Grade_Object_Model();
+        
+        $this->assertEquals($this->baseObj, $this->testObj);
+    }    
 
     /**
      * Test that constructor works as expected
@@ -62,6 +86,128 @@ class D2LWS_Grade_Object_ModelTest extends GenericTestCase
     public function testCreateNewInstanceWithInvalidConstructorArgument()
     {
         $objGrade = new D2LWS_Grade_Object_Model('shouldNotAcceptString');
+    }        
+
+    /**
+     * Test that setID and getID work as expected
+     */
+    public function testSetAndGetID()
+    {
+        
+        // Set the Role ID
+        $this->testObj->setID(99);
+
+        // Assert that a change occurred in the test object
+        $this->assertNotEquals($this->testObj, $this->baseObj);
+
+        // Assert that Role ID field was updated
+        $this->assertEquals(99, $this->testObj->getID());
+
+        // Assert that no other return values were affected
+        $this->_assertModelsSameExcept($this->testObj, $this->baseObj, 'ID');
     }    
 
+    /**
+     * Test that setName and getName work as expected
+     */
+    public function testSetAndGetName()
+    {
+        
+        // Set the Role ID
+        $this->testObj->setName('MyName');
+
+        // Assert that a change occurred in the test object
+        $this->assertNotEquals($this->testObj, $this->baseObj);
+
+        // Assert that Role ID field was updated
+        $this->assertEquals('MyName', $this->testObj->getName());
+
+        // Assert that no other return values were affected
+        $this->_assertModelsSameExcept($this->testObj, $this->baseObj, 'Name');
+    }
+
+    /**
+     * Test that setType and getType work as expected
+     */
+    public function testSetAndGetType()
+    {
+        
+        // Set the Role ID
+        $this->testObj->setType('Calculated');
+
+        // Assert that a change occurred in the test object
+        $this->assertNotEquals($this->testObj, $this->baseObj);
+
+        // Assert that Role ID field was updated
+        $this->assertEquals('Calculated', $this->testObj->getType());
+
+        // Assert that no other return values were affected
+        $this->_assertModelsSameExcept($this->testObj, $this->baseObj, 'Type');
+    }
+
+    /**
+     * Test that setOrgUnitID and getOrgUnitID work as expected
+     */
+    public function testSetAndGetOrgUnitID()
+    {
+        
+        // Set the Role ID
+        $this->testObj->setOrgUnitID(9999);
+
+        // Assert that a change occurred in the test object
+        $this->assertNotEquals($this->testObj, $this->baseObj);
+
+        // Assert that Role ID field was updated
+        $this->assertEquals(9999, $this->testObj->getOrgUnitID());
+
+        // Assert that no other return values were affected
+        $this->_assertModelsSameExcept($this->testObj, $this->baseObj, 'OrgUnitID');
+    }
+ 
+    /**
+     * @dataProvider providerIsTypeMethods
+     * @param type $method Method under test
+     * @param type $expectedType Expected type
+     */
+    public function testIsTypeMethods($type, $trueMethod)
+    {
+        $this->testObj->setType($type);
+        
+        $allMethods = array(
+            'isNumeric',
+            'isPassFail',
+            'isSelectBox',
+            'isText',
+            'isCalculated',
+            'isFormula',
+            'isCalculatedFinalGrade',
+            'isAdjustedFinalGrade',
+            'isCategory'
+        );
+        foreach ( $allMethods as $testMethod )
+        {
+            $testMethod == $trueMethod
+                ? $this->assertTrue($this->testObj->{$testMethod}())
+                : $this->assertFalse($this->testObj->{$testMethod}());
+        }
+    }
+    
+    /**
+     * Data Provider for testIsTypeMethods
+     * @return array 
+     */
+    public function providerIsTypeMethods()
+    {
+        return array(
+            array('Numeric', 'isNumeric'),
+            array('PassFail', 'isPassFail'),
+            array('SelectBox', 'isSelectBox'),
+            array('Text', 'isText'),
+            array('Calculated', 'isCalculated'),
+            array('Formula', 'isFormula'),
+            array('CalculatedFinalGrade', 'isCalculatedFinalGrade'),
+            array('AdjustedFinalGrade', 'isAdjustedFinalGrade'),
+            array('Category', 'isCategory')
+        );
+    }
 }
