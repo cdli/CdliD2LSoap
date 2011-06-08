ed1ae40cbcd5b37c14e5752110d317d374765098
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-06-08 11:24:35 -0230

    Added live-server tests for Instance and Authenticate

---
+++ tests/library/D2LWS/AuthenticateLiveTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Desire2Learn Web Serivces for Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://github.com/adamlundrigan/zfD2L/blob/master/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to adamlundrigan@cdli.ca so we can send you a copy immediately.
+ * 
+ * @copyright  Copyright (c) 2011 Centre for Distance Learning and Innovation
+ * @license    New BSD License 
+ * @author     Adam Lundrigan <adamlundrigan@cdli.ca>
+ * @author     Thomas Hawkins <thawkins@mun.ca>
+ */
+
+/**
+ * PHPUnit test for D2LWS_Authenticate
+ * @author Adam Lundrigan <adamlundrigan@cdli.ca>
+ * 
+ * @group D2LWS
+ * @group D2LWS_Core
+ * @group D2LWS_Live
+ */
+class D2LWS_AuthenticateLiveTest extends GenericTestCase
+{
+
+    /**
+     * Test our ability to receive an authentication token from the server
+     */
+    public function testCanAuthenticateAgainstLiveServer()
+    {
+        $o = $this->_getInstanceManager();
+        $authMgr = $o->getAuthManager();
+        $token = $authMgr->getToken();
+        
+        $this->assertInternalType('string', $token);
+        $this->assertEquals(600, strlen($token));
+    }
+
+}
