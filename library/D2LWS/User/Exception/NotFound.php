1a26c2de8c21d28c431ecb5f1eb8f2aae7c2e61d
Adam Lundrigan <adamlundrigan@cdli.ca>
2011-06-08 10:04:53 -0230

    Updated file-level docblock with license and copyright information

--- library/D2LWS/User/Exception/NotFound.php
+++ library/D2LWS/User/Exception/NotFound.php
@@ -1,11 +1,22 @@
 <?php
-
 /**
  * Desire2Learn Web Serivces for Zend Framework
- * @author Adam Lundrigan <adamlundrigan@cdli.ca>
- * @author Thomas Hawkins <thawkins@mun.ca>
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://github.com/adamlundrigan/zfD2L/blob/master/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to adamlundrigan@cdli.ca so we can send you a copy immediately.
+ * 
+ * @copyright  Copyright (c) 2011 Centre for Distance Learning and Innovation
+ * @license    New BSD License 
+ * @author     Adam Lundrigan <adamlundrigan@cdli.ca>
+ * @author     Thomas Hawkins <thawkins@mun.ca>
  */
-
  
 /**
  * Exception thrown when user account not found in D2L
