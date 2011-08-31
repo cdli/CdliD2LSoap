1fdd1140e1c26f263307fc5a366da89223b54a2b
Adam Lundrigan <adam@lundrigan.ca>
2011-08-31 18:41:58 -0230

    Implemented findByCode for Semester OrgUnit type

--- library/D2LWS/OrgUnit/Semester/API.php
+++ library/D2LWS/OrgUnit/Semester/API.php
@@ -49,4 +49,25 @@ class D2LWS_OrgUnit_Semester_API extends D2LWS_Common
         }
     }
     
+    public function findByCode($code)
+    {
+        $i = $this->getInstance();        
+
+        $result = $i->getSoapClient()
+            ->setWsdl($i->getConfig('webservice.org.wsdl'))
+            ->setLocation($i->getConfig('webservice.org.endpoint'))
+            ->GetSemesterByCode(array(
+                'Code'=>$code
+            ));
+        if ( $result instanceof stdClass && isset($result->Semester) && $result->Semester instanceof stdClass )
+        {
+            $User = new D2LWS_OrgUnit_Semester_Model($result->Semester);
+            return $User;
+        }
+        else
+        {
+            throw new D2LWS_OrgUnit_Semester_Exception_NotFound('Code=' . $code);
+        }
+    }
+    
 }
