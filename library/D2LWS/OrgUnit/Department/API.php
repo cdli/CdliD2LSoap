123266b05f7ca1a02620fdae0ce9d51e8725624a
Adam Lundrigan <adam@lundrigan.ca>
2011-09-19 13:32:36 -0230

    [feature/create] Added save method to D2LWS_OrgUnit_Department_API

--- library/D2LWS/OrgUnit/Department/API.php
+++ library/D2LWS/OrgUnit/Department/API.php
@@ -79,4 +79,42 @@ class D2LWS_OrgUnit_Department_API extends D2LWS_Common
         }
     }
     
+    /**
+     * Persist Department object to Desire2Learn
+     * @param D2LWS_OrgUnit_Department_Model $o Course Offering
+     * @return bool
+     * @throws D2LWS_Soap_Client_Exception on server error
+     */
+    public function save(D2LWS_OrgUnit_Department_Model &$o)
+    {
+        $data = $o->getRawData();        
+        $i = $this->getInstance();     
+        
+        if ( is_null($o->getID()) )
+        {
+            unset($data->OrgUnitId);
+            
+            $result = $i->getSoapClient()
+                ->setWsdl($i->getConfig('webservice.org.wsdl'))
+                ->setLocation($i->getConfig('webservice.org.endpoint'))
+                ->CreateDepartment($data);
+            
+            if ( $result instanceof stdClass && isset($result->Department) )
+            {
+                $o = new D2LWS_OrgUnit_Department_Model($result->Department);
+                return true;
+            }
+        }
+        else
+        {
+            $result = $i->getSoapClient()
+                ->setWsdl($i->getConfig('webservice.org.wsdl'))
+                ->setLocation($i->getConfig('webservice.org.endpoint'))
+                ->UpdateDepartment($data);
+            return ( $result instanceof stdClass );
+        }
+        
+        return false;
+    }
+    
 }
